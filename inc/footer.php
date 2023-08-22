16a6780ba87de787fd968700d880917650c29ce5
unknown <binu@mapleleapgroups.com>
2023-08-22 17:23:26 +0545

    footer part all finished

--- inc/footer.php
+++ inc/footer.php
@@ -1,53 +1,104 @@
 <footer class="main-footer">
     <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="footer-left">
-                    <h6>Good Men Moving Company</h6>
-                    <div class="footer-left-info">
-                        <p class="good-man-moving-p">Good Men Moving Company is a Family-Owned Moving Company in Edmonton serving Edmonton from
-                            last over 25 Years. Our crews are highly trained and experienced professional Edmonton
-                            movers, that is why We pride ourselves on being amongst the most professional and
-                            experienced movers in Edmonton if you are looking for edmonton local movers then u have come
-                            to write website.</p>
+        <div class="footer-up">
+            <div class="row">
+                <div class="col-md-4">
+                    <i class="fa-solid fa-phone"></i>
+                    <a href="tel:780-937-6666">780-937-6666</a>
+                </div>
+                <div class="col-md-4">
+                    <i class="fa-regular fa-envelope"></i>
+                    <a href="#">goodmenmovingco@gmail.com</a>
+                </div>
+                <div class="col-md-4">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <a href="#"> 6825 106 St, Edmonton, AB T6H 2W1</a>
+                </div>
+            </div>
+        </div>
+        <div class="footer-between">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="footer-left">
+                        <h6>Good Men Moving Company</h6>
+                        <div class="footer-left-info">
+                            <p class="good-man-moving-p">Good Men Moving Company is a Family-Owned Moving Company in
+                                Edmonton serving Edmonton from
+                                last over 25 Years. Our crews are highly trained and experienced professional Edmonton
+                                movers, that is why We pride ourselves on being amongst the most professional and
+                                experienced movers in Edmonton if you are looking for edmonton local movers then u have
+                                come
+                                to write website.</p>
+                        </div>
+                        <div class="footer-social-media">
+                            <ul>
+                                <li class="li-h-1"><a href="https://www.facebook.com/profile.php?id=100089283152732"><i
+                                            class="fa-brands fa-facebook-f"></i></a>
+                                </li>
+                                <li><a href="https://www.pinterest.com/goodmenmovingcompany/"><i
+                                            class="fa-brands fa-pinterest-p"></i></a></li>
+                                <li><a href="mail:goodmenmovingco@gmail.com"><i class="fa-regular fa-envelope"></i></a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                    <div class="footer-social-media">
+                </div>
+                <div class="col-md-4">
+                    <div class="footer-mid">
+                        <h6>Quick Links</h6>
+                        <div class="ul-wrapper-box">
+                            <ul class="list-content list-x2">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Services</a></li>
+                                <li><a href="#">Testimonials</a></li>
+                                <li><a href="#">Contacts</a></li>
+                                <li><a href="#">FAQS</a></li>
+                            </ul>
+                        </div>
+                        <div class="row bbb-wrap">
+                            <div class="col-md-6">
+                                <div class="describe-box">
+                                    <h5>Professional <strong>movers</strong></h5>
+                                    <span>24hrs7daysaweek</span>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="bbb-img">
+                                    <img src="assets/img/bbb.png" alt="bbb img">
+                                    <img class="budget-img" src="assets/img/budget-vector-logo.jpg" alt="budget vector">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="footer-right">
+                        <h6>Contact Us</h6>
                         <ul>
-                            <li class="li-h-1"><a href="https://www.facebook.com/profile.php?id=100089283152732"><i
-                                        class="fa-brands fa-facebook-f"></i></a>
+                            <li><a href="#"><i class="fa-solid fa-location-dot"></i> 6825 106 St, Edmonton, AB T6H
+                                    2W1</a>
                             </li>
-                            <li><a href="https://www.pinterest.com/goodmenmovingcompany/"><i
-                                        class="fa-brands fa-pinterest-p"></i></a></li>
-                            <li><a href="mail:goodmenmovingco@gmail.com"><i class="fa-regular fa-envelope"></i></a></li>
+                            <li><a href="tel:780-937-6666"><i class="fa-solid fa-phone"></i> 780-937-6666</a></li>
+                            <li><a href="#"><i class="fa-regular fa-envelope"></i> goodmenmovingco@gmail.com</a></li>
+                            <li><a href="#"><i class="fa-regular fa-clock"></i>Monday-Friday: 9am-9pm Saturday-Sunday:
+                                    9am-7pm</a></li>
                         </ul>
+                        <div class="payment">
+                            <img src="assets/img/visa.png" alt="visa card">
+                            <img src="assets/img/mastercard.png" alt="master card">
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div class="footer-mid">
-                    <h6>Quick Links</h6>
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Services</a></li>
-                        <li><a href="#">Testimonials</a></li>
-                        <li><a href="#">Contacts</a></li>
-                        <li><a href="#">FAQS</a></li>
-                    </ul>
-                </div>
+        </div>
+        <div class="footer-end">
+            <div class="copyright">
+                <span>Copyright © 2023 Good Men Moving | <strong> All Rights Reserved.</strong></span>
             </div>
-            <div class="col-md-4">
-                <div class="footer-right">
-                    <h6>Contact Us</h6>
-                    <ul>
-                        <li><a href="#"><i class="fa-solid fa-location-dot"></i> 6825 106 St, Edmonton, AB T6H 2W1</a>
-                        </li>
-                        <li><a href="tel:780-937-6666"><i class="fa-solid fa-phone"></i> 780-937-6666</a></li>
-                        <li><a href="#"><i class="fa-regular fa-envelope"></i> goodmenmovingco@gmail.com</a></li>
-                        <li><a href="#"><i class="fa-regular fa-clock"></i>Monday-Friday: 9am-9pm Saturday-Sunday:
-                                9am-7pm</a></li>
-                    </ul>
-                </div>
+            <div class="design-maple">
+                <span>Designed and Hosted By: <a href="https://mapleleapgroups.com.np/"><img
+                            src="assets/img/maple-leap-logo.gif" alt="maple design"></a></span>
             </div>
         </div>
     </div>
